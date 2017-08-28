ac4b32333508e1349752c1c9d04e91783a911a12
neliza <nicolasaelf64@gmail.com>
2017-08-28 11:30:25 -0600

    buttonsrendered

---
+++ assets/javascript/gifHomework.js
@@ -0,0 +1,65 @@
+//STEP 1: Set up an array of animals in a variable
+	//for the API to grab initially
+	var animals = [	"polar bear", "chinchilla", "penguin", "whale",
+ 				"frog", "elephant", "lion", "baboon", "bat", 
+ 				"fish", "eagle", "honey bee", "walrus"];
+
+//STEP 2: Set up query URL
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + 
+				animals + "&api_key=e22a26b4f57540b8af89c62d87bb33a6&limit=10";
+
+//STEP 3: Create buttons out of the original array of animals 
+	//to display in the class addedAnimalButtons
+	function renderButtons(){
+		//Delete content inside the addedAnimalsButtons div
+			//So not to have repeat buttons
+		$(".addedAnimalButtons").empty();
+
+		//Loop through the array of animals, then generate buttons for each
+			//animal in the array
+		for (var i = 0; i < animals.length; i++) {
+			$(".addedAnimalButtons").append("<button>" + animals[i] + "</button>");
+		} //End of the for loop
+	}//End of the renderButtons function
+
+	
+//STEP 4: The user can add an animal button to the addedAnimalButton div
+	//using the animalForm
+	$("#add-animal").on("click", function(event){
+		var addedAnimal = $("#animalInput").val();
+	//prevent submit button from trying to submit form
+		event.preventDefault();
+		animals.push(addedAnimal);
+
+	renderButtons();
+	console.log(animals);
+
+	}) //End of the click function
+
+	renderButtons();
+
+//STEP 5: Set up ajax function to request information Giphy
+
+$.ajax({
+	url:queryURL,
+	method:"GET"
+}).done(function(response) {
+
+	console.log(response);
+})
+
+
+//STEP 6: When user clicks animal buttons up top
+	//ten gifs AND their rating will be displayed into the gifDump
+
+
+
+
+//STEP 7: When the user adds an animal using the form, 
+	//A button for the new animal will be created
+	//the new animal is PUSHED into the array of animals
+		//That now has access to the GIPHY API
+
+//STEP 8: Create a function where the player can:
+	//"pause" and "play" the gifs
+	//Apply this function to the animals in the array
