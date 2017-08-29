412fdb6ccf7d36e013b0b86193b915dc7a8bffb4
neliza <nicolasaelf64@gmail.com>
2017-08-29 13:59:29 -0600

    gifs not working

--- assets/javascript/gifHomework.js
+++ assets/javascript/gifHomework.js
@@ -5,33 +5,7 @@ $(document).ready(function() {
  				"frog", "elephant", "lion", "baboon", "bat", 
  				"fish", "eagle", "honey bee", "walrus"];
 
-//STEP 2: Set up ajax function to request information Giphy
-//Register click handler
-	$("button").on("click", function() {
-
-	function displayAnimalGifs () {
-
-	var animal = $(this).attr("data-animal");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + 
-				animal + "&api_key=e22a26b4f57540b8af89c62d87bb33a6&limit=10";
-
-	//THIS IS NOT WORKING
-	$.ajax({
-		url:queryURL,
-		method:"GET"
-	}).done(function(response) {
-		var results = response.data;
-		console.log(this);
-		$("#gifDump").html('<img scr"' + results[0].images.original.url + '">');
-
-		});
-
-	}
-displayAnimalGifs();
-
-})
-
-//STEP 3: Create buttons out of the original array of animals 
+//STEP 2: Create buttons out of the original array of animals 
 	//to display in the class addedAnimalButtons
 	function renderButtons(){
 		//Delete content inside the addedAnimalsButtons div
@@ -41,8 +15,8 @@ displayAnimalGifs();
 		//Loop through the array of animals, then generate buttons for each
 			//animal in the array
 		for (var i = 0; i < animals.length; i++) {
-			var newAnimalButton = $("<button type='button' class='btn btn-success'>");
-			newAnimalButton.attr("data-animal", "data-state", animals[i]);
+			var newAnimalButton = $("<button type='button' id='animalCall' class='btn btn-success' style='margin:10px';>");
+			newAnimalButton.attr("data-state", "data-name", animals[i]);
 			newAnimalButton.text(animals[i]);
 			$(".addedAnimalButtons").append(newAnimalButton);
 		} //End of the for loop
@@ -50,7 +24,7 @@ displayAnimalGifs();
 
 	renderButtons();
 	
-//STEP 4: The user can add an animal button to the addedAnimalButton div
+//STEP 3: The user can add an animal button to the addedAnimalButton div
  	//using the animalForm
 	$("#add-animal").on("click", function(event){
 		var addedAnimal = $("#animalInput").val();
@@ -65,12 +39,36 @@ displayAnimalGifs();
 
 	renderButtons();
 
+//STEP 4: Set up ajax function to request information Giphy
+//Register click handler
+	$("button").on("click", function() {
+
+	function displayAnimalGifs () {
+
+	var animal = $(this).attr("data-name");
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + 
+				animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+	//THIS IS NOT WORKING
+	$.ajax({
+		url:queryURL,
+		method:"GET"
+	}).done(function(response) {
+		var results = response.data;
+		console.log(this);
+		$("#gifDump").html('<img scr"' + results[0].images.original.url + '">');
+
+		});
+	}
+	displayAnimalGifs();
+
+})
 
 //STEP 5: Create a function where the player can:
 	//"pause" and "play" the gifs
 	//Apply this function to the animals in the array
 	$(".addedAnimalButtons").on("click", function() {
-		if(state === "still") {
+		if("data-state" === "still") {
             $(this).attr('data-state', 'animate');
             $(this).attr('src', $(this).attr('data-animate'));
           } else{
@@ -78,8 +76,6 @@ displayAnimalGifs();
             $(this).attr('src', $(this).attr('data-still'));
           }
     })
-
-
 
 }) 
 		
