42348d2b5140986e89935432d5b9a8e729b1c1ab
neliza <nicolasaelf64@gmail.com>
2017-08-29 00:23:02 -0600

    gifs are not showing up- scope problem?

--- assets/javascript/gifHomework.js
+++ assets/javascript/gifHomework.js
@@ -1,12 +1,35 @@
+$(document).ready(function() {
 //STEP 1: Set up an array of animals in a variable
 	//for the API to grab initially
 	var animals = [	"polar bear", "chinchilla", "penguin", "whale",
  				"frog", "elephant", "lion", "baboon", "bat", 
  				"fish", "eagle", "honey bee", "walrus"];
 
-//STEP 2: Set up query URL
+//STEP 2: Set up ajax function to request information Giphy
+//Register click handler
+	$("button").on("click", function() {
+
+	function displayAnimalGifs () {
+
+	var animal = $(this).attr("data-animal");
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + 
-				animals + "&api_key=e22a26b4f57540b8af89c62d87bb33a6&limit=10";
+				animal + "&api_key=e22a26b4f57540b8af89c62d87bb33a6&limit=10";
+
+	//THIS IS NOT WORKING
+	$.ajax({
+		url:queryURL,
+		method:"GET"
+	}).done(function(response) {
+		var results = response.data;
+		console.log(this);
+		$("#gifDump").html('<img scr"' + results[0].images.original.url + '">');
+
+		});
+
+	}
+displayAnimalGifs();
+
+})
 
 //STEP 3: Create buttons out of the original array of animals 
 	//to display in the class addedAnimalButtons
@@ -18,13 +41,17 @@
 		//Loop through the array of animals, then generate buttons for each
 			//animal in the array
 		for (var i = 0; i < animals.length; i++) {
-			$(".addedAnimalButtons").append("<button>" + animals[i] + "</button>");
+			var newAnimalButton = $("<button type='button' class='btn btn-success'>");
+			newAnimalButton.attr("data-animal", animals[i]);
+			newAnimalButton.text(animals[i]);
+			$(".addedAnimalButtons").append(newAnimalButton);
 		} //End of the for loop
 	}//End of the renderButtons function
 
+	renderButtons();
 	
 //STEP 4: The user can add an animal button to the addedAnimalButton div
-	//using the animalForm
+ 	//using the animalForm
 	$("#add-animal").on("click", function(event){
 		var addedAnimal = $("#animalInput").val();
 	//prevent submit button from trying to submit form
@@ -38,28 +65,17 @@
 
 	renderButtons();
 
-//STEP 5: Set up ajax function to request information Giphy
 
-$.ajax({
-	url:queryURL,
-	method:"GET"
-}).done(function(response) {
-
-	console.log(response);
-})
-
-
-//STEP 6: When user clicks animal buttons up top
-	//ten gifs AND their rating will be displayed into the gifDump
-
-
-
-
-//STEP 7: When the user adds an animal using the form, 
-	//A button for the new animal will be created
-	//the new animal is PUSHED into the array of animals
-		//That now has access to the GIPHY API
-
-//STEP 8: Create a function where the player can:
+//STEP 5: Create a function where the player can:
 	//"pause" and "play" the gifs
 	//Apply this function to the animals in the array
+
+
+
+}) 
+		
+
+	
+
+
+
